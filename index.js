79a5466882ab60341a1f88f4612cd13c14bd69ee
Tom MacWright <tom@macwright.org>
2014-07-23 15:25:50 -0400

    Refactorer

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-var fc = { type: 'FeatureCollection', features: [] };
+var count = (process.argv[2] && parseInt(process.argv[2], 10)) || 100;
+var type = process.argv[3] || 'point';
 
-switch (process.argv[2]) {
-    default:
-        for (var i = 0; i < 100; i++) {
-            fc.features.push({
-                type: 'Feature',
-                geometry: point(),
-                properties: {}
-            });
-        }
-        console.log(JSON.stringify(fc));
+switch (type) {
+    case 'point':
+        var features = [];
+        for (var i = 0; i < count; i++) { features.push(feature(point())); }
+        console.log(JSON.stringify(collection(features)));
         break;
 }
 
 function lon() { return (Math.random() - 0.5) * 360; }
 function lat() { return (Math.random() - 0.5) * 180; }
 function point() { return { type: 'Point', coordinates: [lon(), lat()] }; }
+function feature(geom) {
+    return { type: 'Feature', geometry: geom, properties: {} };
+}
+function collection(f) { return { type: 'FeatureCollection', features: f }; }

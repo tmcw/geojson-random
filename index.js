f6bd25ca2c5f906c33b11d7c222c1b7f466c060b
sbma44 <thomas.j.lee@gmail.com>
2014-08-29 19:25:18 -0300

    adding support for simple polygons
    adding polygon test
    
    realized that the angle between the 0th and nth vertex was always just a rounding error. fixed by generating a spare vertex & eventually discarding it
    
    converting tabs to spaces. better handling of numeric command line params.

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-module.exports = function(count, type) {
+module.exports = function(count, type, max_radial_length, num_vertices) {
     switch (type) {
         case 'point':
             var features = [];
             for (var i = 0; i < count; i++) { features.push(feature(point())); }
+            return collection(features);
+
+        case 'polygon':    
+            var features = [];
+
+            for (var i = 0; i < count; i++) {
+                var vertices = [];
+
+                // generate some random numbers
+                var circle_offsets = Array.apply(null, new Array(parseInt(num_vertices + 1))).map(Math.random);             
+
+                // sum them in ascending order
+                circle_offsets.forEach(function(cur, index, arr) {
+                    arr[index] = (index>0) ? cur + arr[index - 1] : cur;
+                });             
+
+                // lurch in a radial fashion
+                circle_offsets.forEach(function(cur, index){                                        
+                    cur = cur * 2 * Math.PI / circle_offsets[circle_offsets.length - 1];
+
+                    var radial_scaler = Math.random();                                              
+                    vertices.push([
+                        radial_scaler * max_radial_length * Math.sin(cur),
+                        radial_scaler * max_radial_length * Math.cos(cur)
+                    ]);             
+                });
+
+                // discard final vertex, close the ring
+                vertices[vertices.length - 1] = vertices[0];
+
+                // center the polygon around something              
+                var hub = [lon(), lat()];               
+                vertices = vertices.map(function(cur, index){
+                    return [cur[0] + hub[0], cur[1] + hub[1]];
+                });
+
+                features.push(feature(polygon([vertices])));
+            }
+
             return collection(features);
     }
 };
@@ -10,7 +49,10 @@ module.exports = function(count, type) {
 function rnd() { return Math.random() - 0.5; }
 function lon() { return rnd() * 360; }
 function lat() { return rnd() * 180; }
+
 function point() { return { type: 'Point', coordinates: [lon(), lat()] }; }
+function polygon(coordinates) { return { type: 'Polygon', coordinates: coordinates }; }
+
 function feature(geom) {
     return { type: 'Feature', geometry: geom, properties: {} };
 }
--- test.js
+++ test.js
@@ -3,5 +3,6 @@ var test = require('tape'),
 
 test('geojson-random', function(t) {
     t.equal(geojsonRandom(100, 'point').features.length, 100, '100 points');
+    t.equal(geojsonRandom(100, 'polygon', 1, 20).features.length, 100, '100 polygons');
     t.end();
 });

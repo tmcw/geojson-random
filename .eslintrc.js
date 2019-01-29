edabacf6d2691c7bedcb40903554a5ee58fa65b2
Tom MacWright <tom@macwright.org>
2019-01-28 23:12:09 -0800

    refactor: Prettier style, define some undefineds.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module"
+  },
+  rules: {
+    "prefer-const": ["error"]
+  }
+};
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var from = require('from2');
+const from = require("from2");
 
 module.exports = function() {
-    throw new Error('call .point(), .lineString(), or .polygon() instead');
+  throw new Error("call .point(), .lineString(), or .polygon() instead");
 };
 
 function position(bbox) {
-    if (bbox) return coordInBBBOX(bbox);
-    else return [lon(), lat()];
+  if (bbox) return coordInBBBOX(bbox);
+  else return [lon(), lat()];
 }
 
 module.exports.position = position;
 
 module.exports.point = function(count, bbox) {
-    var features = [];
-    for (var i = 0; i < count; i++) {
-        features.push(feature(bbox ? point(position(bbox)) : point()));
-    }
-    return collection(features);
+  const features = [];
+  for (let i = 0; i < count; i++) {
+    features.push(feature(bbox ? point(position(bbox)) : point()));
+  }
+  return collection(features);
 };
 
 module.exports.pointStream = function(count, bbox) {
@@ -29,123 +29,133 @@ module.exports.pointStream = function(count, bbox) {
   });
 };
 
-module.exports.polygon = function(count, num_vertices, max_radial_length, bbox) {
-    if (typeof num_vertices !== 'number') num_vertices = 10;
-    if (typeof max_radial_length !== 'number') max_radial_length = 10;
-    var features = [];
-    for (i = 0; i < count; i++) {
-        var vertices = [],
-            circle_offsets = Array.apply(null,
-                new Array(num_vertices + 1)).map(Math.random);
+module.exports.polygon = function(
+  count,
+  num_vertices,
+  max_radial_length,
+  bbox
+) {
+  if (typeof num_vertices !== "number") num_vertices = 10;
+  if (typeof max_radial_length !== "number") max_radial_length = 10;
+  const features = [];
+  for (let i = 0; i < count; i++) {
+    let vertices = [];
+    const circle_offsets = Array.apply(null, new Array(num_vertices + 1)).map(
+      Math.random
+    );
 
-        circle_offsets.forEach(sumOffsets);
-        circle_offsets.forEach(scaleOffsets);
-        vertices[vertices.length - 1] = vertices[0]; // close the ring
+    circle_offsets.forEach(function sumOffsets(cur, index, arr) {
+      arr[index] = index > 0 ? cur + arr[index - 1] : cur;
+    });
+    circle_offsets.forEach(function scaleOffsets(cur) {
+      cur = (cur * 2 * Math.PI) / circle_offsets[circle_offsets.length - 1];
+      const radial_scaler = Math.random();
+      vertices.push([
+        radial_scaler * max_radial_length * Math.sin(cur),
+        radial_scaler * max_radial_length * Math.cos(cur)
+      ]);
+    });
+    vertices[vertices.length - 1] = vertices[0]; // close the ring
 
-        // center the polygon around something
-        vertices = vertices.map(vertexToCoordinate(position(bbox)));
-        features.push(feature(polygon([vertices])));
-    }
+    // center the polygon around something
+    vertices = vertices.map(vertexToCoordinate(position(bbox)));
+    features.push(feature(polygon([vertices])));
+  }
 
-    function sumOffsets(cur, index, arr) {
-        arr[index] = (index > 0) ? cur + arr[index - 1] : cur;
-    }
-
-    function scaleOffsets(cur, index) {
-        cur = cur * 2 * Math.PI / circle_offsets[circle_offsets.length - 1];
-        var radial_scaler = Math.random();
-        vertices.push([
-            radial_scaler * max_radial_length * Math.sin(cur),
-            radial_scaler * max_radial_length * Math.cos(cur)
-        ]);
-    }
-
-    return collection(features);
+  return collection(features);
 };
 
-module.exports.lineString = function(count, num_vertices, max_length, max_rotation, bbox) {
-    if (typeof num_vertices !== 'number' || num_vertices < 2) num_vertices = 10;
-    if (typeof max_length !== 'number') max_length = 0.0001;
-    if (typeof max_rotation !== 'number') max_rotation = Math.PI / 8;
+module.exports.lineString = function(
+  count,
+  num_vertices,
+  max_length,
+  max_rotation,
+  bbox
+) {
+  if (typeof num_vertices !== "number" || num_vertices < 2) num_vertices = 10;
+  if (typeof max_length !== "number") max_length = 0.0001;
+  if (typeof max_rotation !== "number") max_rotation = Math.PI / 8;
 
-    var features = [];
-    for (i = 0; i < count; i++) {
-        var startingPoint = position(bbox);
-        var vertices = [startingPoint];
-        for (var j = 0; j < num_vertices - 1; j++) {
-            var priorAngle = (j === 0)
-            ? Math.random() * 2 * Math.PI
-            : Math.tan(
+  const features = [];
+  for (let i = 0; i < count; i++) {
+    const startingPoint = position(bbox);
+    const vertices = [startingPoint];
+    for (let j = 0; j < num_vertices - 1; j++) {
+      const priorAngle =
+        j === 0
+          ? Math.random() * 2 * Math.PI
+          : Math.tan(
               (vertices[j][1] - vertices[j - 1][1]) /
-              (vertices[j][0] - vertices[j - 1][0])
-            )
-            var angle = priorAngle + (Math.random() - 0.5) * max_rotation * 2;
-            var distance = Math.random() * max_length;
-            vertices.push([
-                vertices[j][0] + distance * Math.cos(angle),
-                vertices[j][1] + distance * Math.sin(angle)
-            ]);
-        }
-        features.push(feature(lineString(vertices)));
+                (vertices[j][0] - vertices[j - 1][0])
+            );
+      const angle = priorAngle + (Math.random() - 0.5) * max_rotation * 2;
+      const distance = Math.random() * max_length;
+      vertices.push([
+        vertices[j][0] + distance * Math.cos(angle),
+        vertices[j][1] + distance * Math.sin(angle)
+      ]);
     }
+    features.push(feature(lineString(vertices)));
+  }
 
-    return collection(features);
+  return collection(features);
 };
-
 
 function vertexToCoordinate(hub) {
-    return function(cur, index) { return [cur[0] + hub[0], cur[1] + hub[1]]; };
+  return function(cur) {
+    return [cur[0] + hub[0], cur[1] + hub[1]];
+  };
 }
 
-function rnd() { return Math.random() - 0.5; }
-function lon() { return rnd() * 360; }
-function lat() { return rnd() * 180; }
+function rnd() {
+  return Math.random() - 0.5;
+}
+function lon() {
+  return rnd() * 360;
+}
+function lat() {
+  return rnd() * 180;
+}
 
 function point(coordinates) {
-    return {
-        type: 'Point',
-        coordinates: coordinates || [lon(), lat()]
-    };
+  return {
+    type: "Point",
+    coordinates: coordinates || [lon(), lat()]
+  };
 }
 
 function coordInBBBOX(bbox) {
-    return [
-        (Math.random() * (bbox[2] - bbox[0])) + bbox[0],
-        (Math.random() * (bbox[3] - bbox[1])) + bbox[1]];
-}
-
-function pointInBBBOX() {
-    return {
-        type: 'Point',
-        coordinates: [lon(), lat()]
-    };
+  return [
+    Math.random() * (bbox[2] - bbox[0]) + bbox[0],
+    Math.random() * (bbox[3] - bbox[1]) + bbox[1]
+  ];
 }
 
 function polygon(coordinates) {
-    return {
-        type: 'Polygon',
-        coordinates: coordinates
-    };
+  return {
+    type: "Polygon",
+    coordinates: coordinates
+  };
 }
 
 function feature(geom) {
-    return {
-        type: 'Feature',
-        geometry: geom,
-        properties: {}
-    };
+  return {
+    type: "Feature",
+    geometry: geom,
+    properties: {}
+  };
 }
 
 function collection(f) {
-    return {
-        type: 'FeatureCollection',
-        features: f
-    };
+  return {
+    type: "FeatureCollection",
+    features: f
+  };
 }
 
 function lineString(coordinates) {
-    return {
-      type: 'LineString',
-      coordinates: coordinates
-    };
+  return {
+    type: "LineString",
+    coordinates: coordinates
+  };
 }
